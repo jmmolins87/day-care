61371656d2f434ef0ede5002f2236a8fb444d022
jmmcDev/Sign <35576283+jmmolins87@users.noreply.github.com>
2026-09-20 17:29:18 +0200

    Add ActivatePage component to render ActivateForm for account activation

---
+++ app/activate/page.tsx
@@ -0,0 +1,9 @@
+import ActivateForm from "@/app/components/ActivateForm";
+
+export default function ActivatePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#FBF4EC] p-[40px]">
+      <ActivateForm />
+    </div>
+  );
+}
